3578bdb6a8222e1a533136002f8ab1f194461795
Lisachard <lisa.achard@ynov.com>
2022-11-08 17:06:26 +0100

    Cleaning CSS and HTML, adding full header : finished

--- template/heade.php
+++ template/heade.php
@@ -1,3 +1,7 @@
+<?php
+session_start()
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,6 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.typekit.net/bdt8ycj.css">  
     <link rel="stylesheet" href="../assets/header.css">
     <title>Header</title>
 </head>
@@ -12,11 +17,13 @@
 <body>
     <header class="site-header">
         <div class="wrapper site-header__wrapper">
-            <a href="#" class="brand"><img src="../assets/img/HabitudeLogo.png" alt="Brand Logo" /></a>
-            <nav class="nav"></nav>
+            <a class="brand"><img src="../assets/img/HabitudeLogo.png" alt="Brand Logo"/></a>
+            <p>Classement</p>
+            <p>En savoir +</p>
+            <nav class="nav"><img src="../assets/img/Notification.png" alt="Notification logo"></nav>
+            <img src="<?php echo $_SESSION['profile_picture'] . "60/" . $_SESSION['pseudo']; ?>">
         </div>
     </header>
 </body>
-
 
 </html>

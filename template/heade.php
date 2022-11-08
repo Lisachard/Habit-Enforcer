5c28fccb88449662cd5f2a9cd1fe776fbdf48027
Lisachard <lisa.achard@ynov.com>
2022-11-08 16:12:54 +0100

    Starting header : clean

---
+++ template/heade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/header.css">
+    <title>Header</title>
+</head>
+
+<body>
+    <header class="site-header">
+        <div class="wrapper site-header__wrapper">
+            <a href="#" class="brand"><img src="../assets/img/HabitudeLogo.png" alt="Brand Logo" /></a>
+            <nav class="nav"></nav>
+        </div>
+    </header>
+</body>
+
+
+</html>

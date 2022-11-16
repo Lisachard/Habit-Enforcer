f3a9db162c90e6299223b866f74dc092f483e790
Lisachard <lisa.achard@ynov.com>
2022-11-16 14:43:01 +0100

    Stylizing HTML format

--- template/profile.php
+++ template/profile.php
@@ -9,7 +9,6 @@ session_start()
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.typekit.net/bdt8ycj.css">  
     <link rel="stylesheet" href="../assets/profile.css">
     <title>Profile</title>
 </head>
@@ -19,8 +18,8 @@ session_start()
         <div class="wrapper site-header__wrapper">
             <img class="brand" src="../assets/img/HabitudeLogo.png" alt="Brand Logo"/>
             <a href="heade.php">Home</a>
-            <p>Classement</p>
-            <p>En savoir +</p>
+            <p class="textHeader">Classement</p>
+            <p class="textHeader">En savoir +</p>
             <img src="../assets/img/Notification.png" alt="Notification logo">
         </div>
     </header>

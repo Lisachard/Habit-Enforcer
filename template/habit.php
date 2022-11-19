f1a8187afca4287fab09ad5d8b6b7ed43438e175
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-11-19 23:28:52 +0100

    add habit

--- template/habit.php
+++ template/habit.php
@@ -8,13 +8,34 @@ if (isset($_POST['addHabit'])) {
 }
 ?>
 
-<form action="habit.php" method="post" style="visibility:visible;">
-    <input type="text" name="label" required>
-    <input type="text" name="difficulty" required>
-    <input type="color" name="color" required   >
-    <input type="submit" name="addHabit">
-</form>
+<?php if ($bdd->alreadyCreateHabit()) : ?>
+    <h1>You have already add a habit today, come tomorrow ^^</h1>
+<?php else : ?>
+    <form action="habit.php" method="post" style="visibility:visible;">
+        <input type="text" name="label" placeholder="Activity" required>
+        <select type="periodicities" name="periodicity" required>
+            <option selected hidden>Periodicity</option>
+            <option value="daily">Daily</option>
+            <option value="weekly">Weekly</option>
+        </select>
+        <select type="difficulties" name="difficulty" required>
+            <option selected hidden>Difficulty</option>
+            <option value="1">⭐</option>
+            <option value="2">⭐⭐</option>
+            <option value="3">⭐⭐⭐</option>
+            <option value="4">⭐⭐⭐⭐</option>
+            <option value="5">⭐⭐⭐⭐⭐</option>
+        </select>
+        <input class="box" type="color" name="color" required>
+        <input class="box" type="submit" name="addHabit">
+    </form>
+<?php endif; ?>
 
-<?php
-$bdd->readHabit();
-?>
+<?php foreach ($bdd->readHabit() as $habit) : ?>
+    <div class="card" style="border-top: solid <?php echo $habit['color'] ?>; width: 30vw;">
+        <h1><?php echo $habit['label'] ?></h1>
+        <h2><?php echo str_repeat("⭐",  $habit['difficulty']) ?></h2>
+    </div>
+<?php endforeach ?>
+
+<script src="../script/test.js"></script>
--- template/home.php
+++ template/home.php
@@ -19,12 +19,24 @@
         <div class="board">
             <section class="left">
                 <?php if (isset($_SESSION['party_id'])) : ?>
-                    <h1>Bienvenue dans le groupe n°<?php echo $_SESSION['party_id'] ?></h1>
+                    <h1>Bienvenue dans le groupe <?php echo $_SESSION['party_name'] ?></h1>
                 <?php else : ?>
                     <form action="home.php" method="post">
+                        <input type="text" name="partyName">
                         <input type="submit" name="createParty" value="Create a Party">
                         <input type="submit" name="checkInvitation" value="Check Invitation">
                     </form>
+                    <form action="./home.php" method="post">
+                        <?php for ($i = 0; $i < count($_SESSION['invitation_party_id']); $i++) : ?>
+                            <?php if ($_SESSION['invitation_party_id'][$i] != NULL) : ?>
+                                <div class="card">
+                                    <?php echo $_SESSION['invitation_party_name'][$i] ?>
+                                    <button type="submit" name="party" value="<?php echo $_SESSION['invitation_party_id'][$i]?>">Join</button>
+                                </div>
+                            <?php endif ?>
+                        <?php endfor ?>
+                    </form>
+
                 <?php endif; ?>
             </section>
             <section class="right">
@@ -36,11 +48,11 @@
                                 <input type="text" name="searching" id="searchbar" value="<?php echo $_GET['searching'] ?>" placeholder="Search your Friends here">
                             </form>
                             <form action="home.php?makeInvitation=true&searching=<?php echo $_GET['searching'] ?>" method="post">
-                            <?php
-                            if (isset($_GET['searching'])) {
-                                $bdd->searchFriend();
-                            }
-                            ?>
+                                <?php
+                                if (isset($_GET['searching'])) {
+                                    $bdd->searchFriend();
+                                }
+                                ?>
                             </form>
 
                         </div>
